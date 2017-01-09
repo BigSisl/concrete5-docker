24e7f743de4e3057d8a2e5b29b9f977ca796dde3
Lucien Zürcher <l.zuercher@tincan.ch>
2017-01-09 09:27:32 +0100

    feat: prepare updated script

---
+++ scripts/update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Updates the internal data with an other concrete5 development or production
+# server by syncronising the database and persistent data storage (mainly files)
+# Specificaly for concrete5 application:
+# download
+# {FTP:FOLDER}/application/files
+# download DB and importing Data to local server
+# {SERVER}:{PORT} {DBNAME} {DBPW} {DBUSR}
+SERVER=
+PORT=
+DBNAME=
+DBUSR=
+DBPW=
+
+# Prepare external Database
+
+
+# Syncronise the database
+
+# Download files
