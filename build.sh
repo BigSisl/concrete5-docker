e1b029de49ded334635f6448642d3db504f44c2a
Lucien Zürcher <l.zuercher@tincan.ch>
2017-01-09 08:32:14 +0100

    feat: updated variables and doc to version 8.03

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ rm -rf src
 
 git clone -b $C5_VERSION --single-branch https://github.com/concrete5/concrete5.git ./src
 
-docker build . --build-arg CONCRETE5_VERSION=$C5_VERSION -t
+# be sure to convert docker-entrypoint & src/concrete/bin/concrete5 on windows to unix
+# endings (dos2unix.exe) -> git could be checking out the file wrong
+
+docker build . --build-arg CONCRETE5_VERSION=$C5_VERSION -t concrete5/$C5_VERSION
