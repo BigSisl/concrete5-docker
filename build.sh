02a21f6af30bd7746948387dd0ace64dce53dcf4
Lucien Zürcher <l.zuercher@tincan.ch>
2017-01-10 12:07:24 +0100

    feat: doc with dos2unix command added

--- build.sh
+++ build.sh
@@ -9,5 +9,6 @@ git clone -b $C5_VERSION --single-branch https://github.com/concrete5/concrete5.
 
 # be sure to convert docker-entrypoint & src/concrete/bin/concrete5 on windows to unix
 # endings (dos2unix.exe) -> git could be checking out the file wrong
+# Use -> find . -type f -exec dos2unix {} \; # for whole directory
 
 docker build . --build-arg CONCRETE5_VERSION=$C5_VERSION -t concrete5/$C5_VERSION
