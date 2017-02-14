46568769250e19bf7780b6c8daba225cd9eca20c
Lucien Zürcher <l.zuercher@tincan.ch>
2017-02-14 16:44:41 +0100

    feat: updated to version 8.1.0

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-C5_VERSION=8.0.3
+C5_VERSION=8.1.0
 
 # download sourcecode
 rm -rf src
@@ -10,4 +10,5 @@ git clone -b $C5_VERSION --single-branch https://github.com/concrete5/concrete5.
 # be sure to convert docker-entrypoint & src/concrete/bin/concrete5 on windows to unix
 # endings (dos2unix.exe) -> git could be checking out the file wrong
 
-docker build . --build-arg CONCRETE5_VERSION=$C5_VERSION -t concrete5/$C5_VERSION
+docker-compose up
+#docker build . --build-arg CONCRETE5_VERSION=$C5_VERSION -t concrete5/$C5_VERSION
